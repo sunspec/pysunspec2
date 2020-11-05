0ee5af20a27dafdbb4c548c1a82191a84d2f18a4
shelcrow <sheldon.b.crow@gmail.com>
2020-11-05 11:01:11 -0800

    1.0.2 update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 
 setup(
     name='pysunspec2',
-    version='1.0.1',
+    version='1.0.2',
     description='Python SunSpec Tools',
     author='SunSpec Alliance',
     author_email='support@sunspec.org',
