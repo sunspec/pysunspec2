5bdbde9f53bf08f7b55492cbc23e58e21ea70417
shelcrow <sheldon.b.crow@gmail.com>
2021-06-07 11:24:57 -0700

    #40 - Added suns.py CLI utility

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     url='https://sunspec.org/',
     packages=['sunspec2', 'sunspec2.modbus', 'sunspec2.file', 'sunspec2.tests'],
     package_data={'sunspec2.tests': ['test_data/*'], 'sunspec2': ['models/json/*']},
+    scripts=['scripts/suns.py'],
     python_requires='>=3.5',
     extras_require={
       'serial': ['pyserial'],
